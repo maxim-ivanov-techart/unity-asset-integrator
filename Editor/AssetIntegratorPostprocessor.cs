9c7b93555d7b20df9ce55a9237461d77dbea8b6c
Maxim Ivanov <maxim.ivanov.contact@proton.me>
2026-06-11 07:54:05 +0300

    feat: preset for fbx

--- Editor/AssetIntegratorPostprocessor.cs
+++ Editor/AssetIntegratorPostprocessor.cs
@@ -7,14 +7,23 @@ public class AssetIntegratorPostprocessor : AssetPostprocessor
 {
     private void OnPreprocessTexture()
     {
+        ApplyPresetForAsset(assetPath);
+    }
+
+    private void OnPreprocessModel()
+    {
+        ApplyPresetForAsset(assetPath);
+    }
+
+    private void ApplyPresetForAsset(string assetPath)
+    {
         if (AssetIntegratorSession.CurrentConfig == null)
         {
             return;
         }
         
-        string textureName = Path.GetFileNameWithoutExtension(assetPath);
         string extension = Path.GetExtension(assetPath);
-
+        
         foreach (var rule in AssetIntegratorSession.CurrentConfig.rules)
         {
             if (rule.extensions.Contains(extension))
